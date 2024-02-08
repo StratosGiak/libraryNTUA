2b7585ee26c7f5582e2362f126073fdf51d03f2d
StratosGiak <efstratios.giak@gmail.com>
2024-02-08 23:07:48 +0200

    initial commit, sample project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.stratos.giak.libraryntua {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.stratos.giak.libraryntua to javafx.fxml;
+    exports com.stratos.giak.libraryntua;
+}
